6b569ef41006b47ba50068a8bc09a081cb165328
rrrrxc <2585692124@qq.com>
2022-11-27 09:15:48 +0800

    js字符串

---
+++ 04/js基本数据类型/src/js/callback/string.js
@@ -0,0 +1,17 @@
+const url='https:www.baidu.com/s?wd=vue&rsv_spt=1&rsv_iqid=0xa5d93f9e0009e6e3&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_dl=tb&rsv_sug3=4&rsv_sug1=1&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&prefixsug=vue&rsp=8&inputT=706&rsv_sug4=7723'
+
+function urlParser(url) 
+{
+    const resultSet ={}
+   
+     url.split('?')[1]
+        .split('&')
+        .forEach(str => {
+            const pair = str.split('=');
+            resultSet [pair[0]]=pair[1];
+            
+            
+        })
+    return resultSet;
+}
+console.log(urlParser(url))
