8a25e9b4f9d91d0b81d17979e52994664cbbc877
rxc <2585692124@qq.com>
2022-12-20 14:55:10 +0800

    http请求

--- package-learn/index.js
+++ package-learn/index.js
@@ -6,20 +6,20 @@ import /** 自定义的名字 */ http from /** 包的名字*/ 'axios'
 http.get(/** 2.请求的路径(看服务端)*/ 'http://localhost:8080/user', /** 请求的配置 */{
     // 3.请求头部字段(看服务端)
     headers: {
-        pjw: 'pjw'
+        rxc: 'rxc'
     }
 }).then(res => console.log(res.data))
 
 // url 传递需要注意的问题
 // 协议 + :// + 域名(主机ip地址) + 端口号 + 资源相对路径 
 // http://localhost:80/user
-const username = 'pjw'
+const username = 'rxc'
 const password = '123456'
 
 async function main() {
     const result = await http.get(`http://localhost:8080/user`, {
         headers: {
-            pjw: 'pjw'
+            pjw: 'rxc'
         },
         query: {
             password,
